a620db50a929e3981190df61072416b2952ac29e
verbalk <verbalk89@naver.com>
2020-07-14 19:37:00 +0900

    세팅

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LogoType>
+        <p>NASA PROJECT</p>
+      </LogoType>
+      <Container>
+        <Card>1</Card>
+        <Card>2</Card>
+        <Card>3</Card>
+      </Container>
     </div>
   );
 }
+
+const LogoType = styled.header`
+  background: #20232a;
+  padding: 8px;
+  font-size: 12px;
+  color: white;
+`;
+
+const Container = styled.div`
+  clear: both;
+`;
+
+const Card = styled.div`
+  width: 31.3%;
+  background: white;
+  float: left;
+  margin: 5px;
+`;
 
 export default App;

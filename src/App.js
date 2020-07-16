04dd0c3a37afe204797ecee5328d4fe4771236a9
verbalk <verbalk89@naver.com>
2020-07-16 20:46:37 +0900

    0706스터디

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import { Header } from './components/header';
 import styled from 'styled-components';
 
 export function App() {
+  const [images, setImages] = useState([]);
+
+  const getImages = () => {
+    axios
+      .get(
+        'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=RAgi4NFaaCnMPmtH9axvbxuKxPlUw12mM4qiPL0e'
+      )
+      .then((e) => {
+        setImages(e.data.photos);
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <div>
       <Header />
